d07e90b0dc78b29f1a7a118cabc8c3da9d349f03
smendezbravo <sara.mendez489@comunidadunir.net>
2025-05-13 19:42:02 +0200

    landing + styles

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import '../styles/Landing.css'
+
 export default function Landing() {
+  const navigate = useNavigate();
+
+  const handlAccess = () => {
+    navigate('/home');
+  }
   return (
-    <div>
+    <div className="landing">
       <h1>Bienvenido a Relatos de Papel</h1>
-      <p>Redirigiendo a la página principal en unos segundos...</p>
+      <p>Tu libreria digital de confianza</p>
+      <button onClick={handlAccess}> Acceder </button>
     </div>
   );
 }

fdb79d9aa0b6d9cdfaf0bf70ed6537ba4425c7d7
smendezbravo <sara.mendez489@comunidadunir.net>
2025-05-13 20:03:40 +0200

    redirigir a homea los 5seg

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,16 @@
 import { useNavigate } from "react-router-dom";
 import '../styles/Landing.css'
+import { useEffect } from "react";
 
 export default function Landing() {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigate('/home');
+    }, 5000); //5000 = 5seg
+    return () => clearTimeout(timeout);
+  }, [navigate]);
 
   const handlAccess = () => {
     navigate('/home');

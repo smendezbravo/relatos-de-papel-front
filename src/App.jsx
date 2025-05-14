56fc59837593b88b8452b2d2f842458a4cf7d895
mariafp922 <maria.frances697@comunidadunir.net>
2025-05-14 19:59:57 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Home from './pages/Home';
-import BookDetails from './pages/BookDetails';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import BookDetails from "./pages/BookDetails";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       {/* en un futuro hay que quitar el estilo inline */}
-      <main style={{ paddingTop: '80px', paddingBottom: '80px' }}>
+      <main style={{ paddingTop: "80px", paddingBottom: "80px" }}>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/book/:id" element={<BookDetails />} />
+          <Route path="/libro/:id" element={<BookDetails />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
         </Routes>

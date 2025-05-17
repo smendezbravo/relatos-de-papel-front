9de2e0a577fd649dcf8709acb72422e0633b0cfd
smendezbravo <sara.mendez489@comunidadunir.net>
2025-05-17 21:28:15 +0200

    Popup de inicio

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,25 +1,45 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import '../styles/Landing.css'
-import { useEffect } from "react";
+import { Modal } from "react-bootstrap";
+import "../styles/Landing.css";
 
 export default function Landing() {
   const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      navigate('/home');
-    }, 5000); //5000 = 5seg
+      setShowModal(true);
+
+      // redirigimos despues del popup
+      const redirect = setTimeout(() => {
+        navigate("/home");
+      }, 1000);
+
+      return () => clearTimeout(redirect);
+    }, 4000);
+
     return () => clearTimeout(timeout);
   }, [navigate]);
 
   const handlAccess = () => {
     navigate('/home');
-  }
+  };
+
   return (
-    <div className="landing">
-      <h1>Bienvenido a Relatos de Papel</h1>
-      <p>Tu libreria digital de confianza</p>
-      <button onClick={handlAccess}> Acceder </button>
-    </div>
+    <>
+      <div className="landing">
+        <h1>Bienvenido a Relatos de Papel</h1>
+        <p>Tu librería digital de confianza</p>
+        <button onClick={handlAccess}>Acceder</button>
+      </div>
+
+      {/* Popup*/}
+      <Modal show={showModal} centered backdrop="static" keyboard={false}>
+        <Modal.Body>
+          Redirigiendo a la página principal...
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }

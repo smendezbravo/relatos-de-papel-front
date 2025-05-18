326e9e8be4b30556535f0aa39b9c032e6b1813ac
Kevin Hernández <60586063+kevinhKit@users.noreply.github.com>
2025-05-18 12:27:47 -0600

    paso de mas parametros de informacion de libro para consistencia en carrito de compras

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -11,17 +11,18 @@ export default function BookDetails() {
   if (!LibroFiltrado) {
     return <div><h2>Libro no encontrado</h2></div>;
   }
-
   return (
     <div>
       <h2>Detalles del libro</h2>
       <BookInformation
+        BookId={LibroFiltrado.id}
         BookTitle={LibroFiltrado.titulo}
         BookAuthor={LibroFiltrado.autor}
         BookPrice={LibroFiltrado.precio}
         BookDescription={LibroFiltrado.descripcion}
         BookImage={LibroFiltrado.imagen}
         BookYear={LibroFiltrado.anio}
+        BookQualification={LibroFiltrado.calificacion}
       />
     </div>
   );

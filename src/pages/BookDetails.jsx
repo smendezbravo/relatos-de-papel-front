548a7921cef657da5e52e249ade1a415ce8f4356
mariafp922 <maria.frances697@comunidadunir.net>
2025-05-19 15:33:59 +0200

    Cambio navigate

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -1,18 +1,27 @@
 import { useParams } from "react-router-dom";
 import BookInformation from "../components/BookInformation";
 import libros from "../data/libros";
+import { useNavigate } from "react-router-dom";
 
 export default function BookDetails() {
   const { id } = useParams();
+
+  const navigate = useNavigate();
   const LibroFiltrado = libros.find(
     (libro) => libro.id.toString() === id.toString()
   );
 
   if (!LibroFiltrado) {
-    return <div><h2>Libro no encontrado</h2></div>;
+    return (
+      <div>
+        <h2>Libro no encontrado</h2>
+      </div>
+    );
   }
   return (
     <div>
+      <button onClick={() => navigate("/home")}>Seguir comprando</button>
+
       <h2>Detalles del libro</h2>
       <BookInformation
         BookId={LibroFiltrado.id}

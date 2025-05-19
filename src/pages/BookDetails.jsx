6c2fc784841139c3e665594ca4bc7ab0d6cc26fd
Ivan_Caro <ivan-caro-123@hotmail.com>
2025-05-19 10:13:34 -0500

    Restaura proyecto funcional y reconecta con el repositorio original

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -1,29 +1,22 @@
 import { useParams } from "react-router-dom";
-import BookInformation from "../components/BookInformation";
 import libros from "../data/libros";
+import BookInformation from "../components/BookInformation";
 
 export default function BookDetails() {
   const { id } = useParams();
-  const LibroFiltrado = libros.find(
-    (libro) => libro.id.toString() === id.toString()
-  );
+  const libro = libros.find((libro) => libro.id.toString() === id.toString());
 
-  if (!LibroFiltrado) {
-    return <div><h2>Libro no encontrado</h2></div>;
+  if (!libro) {
+    return (
+      <div className="container py-5">
+        <h2>Libro no encontrado</h2>
+      </div>
+    );
   }
+
   return (
-    <div>
-      <h2>Detalles del libro</h2>
-      <BookInformation
-        BookId={LibroFiltrado.id}
-        BookTitle={LibroFiltrado.titulo}
-        BookAuthor={LibroFiltrado.autor}
-        BookPrice={LibroFiltrado.precio}
-        BookDescription={LibroFiltrado.descripcion}
-        BookImage={LibroFiltrado.imagen}
-        BookYear={LibroFiltrado.anio}
-        BookQualification={LibroFiltrado.calificacion}
-      />
+    <div className="container py-5">
+      <BookInformation libro={libro} />
     </div>
   );
 }

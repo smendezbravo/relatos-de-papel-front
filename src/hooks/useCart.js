80aeb26f0dc8619608174e47f02ea412a41682b5
smendezbravo <sara.mendez489@comunidadunir.net>
2025-05-20 15:30:27 +0200

    throwerror changed

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -4,7 +4,7 @@ import { CartContext } from '../context/SimpleCartContext.jsx';
 const useCart = () => {
   const context = useContext(CartContext);
   if (!context) {
-    throw new Error('useCart debe usarse dentro de SimpleCartProvider');
+    throw new Error('useCart error');
   }
   return context;
 };

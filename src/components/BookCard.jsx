6360fe4f9778b6c3b54f3e4a614a64c84f2d96fa
Ivan_Caro <ivan-caro-123@hotmail.com>
2025-05-20 12:14:07 -0500

    ajuste de bookcard para diseño

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { CartContext } from '../context/SimpleCartContext';
+import { CartContext } from './SimpleCartContext';
 import "../styles/BookCard.css";
 
 const BookCard = ({ libro }) => {
